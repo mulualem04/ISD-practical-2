63bd204c1e7b567373cb8e6cc59889453bc83628
Mulualem Kahssay <abebaaregawi03@gmail.com>
2016-11-24 22:27:22 +0000

    This is ISD practical 2

---
+++ practical2Q4.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("I am in my ISD class right now")
+# inorder to make multiple command we create new file
---
+++ practical2Q5.py
@@ -0,0 +1,5 @@
+name=input("Hi there, What is your name? ")
+# the variable 'name' stores a piece of data 
+print("Hello " + name)
+print("How are you")
+
